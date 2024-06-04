c8dbc81c6f994e7694829daf615a61d9f67af560
Pranay Prasad V <pranay.prasad@celllabs.com>
2024-06-04 17:42:35 +0530

    Update JoystickBase with new JoystickArrows.

--- lib/src/joystick_base.dart
+++ lib/src/joystick_base.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_joystick/src/joystick_arrow.dart';
 import 'joystick.dart';
 
 class JoystickBase extends StatelessWidget {
@@ -37,41 +37,45 @@ class JoystickBase extends StatelessWidget {
           )
         ],
       ),
-      child: CustomPaint(
-        painter: _JoystickBasePainter(
-          arrowsColor: arrowsColor ?? color,
-          color: color,
-          drawArrows: drawArrows,
-          mode: mode,
-          withBorderCircle: withBorderCircle,
-        ),
+      child: Stack(
+        children: [
+          SizedBox(
+            width: size,
+            height: size,
+            child: CustomPaint(
+              painter: _JoystickBasePainter(
+                color: color,
+                mode: mode,
+                withBorderCircle: withBorderCircle,
+              ),
+            ),
+          ),
+          if (drawArrows)
+            JoystickArrows(
+              arrowsColor: arrowsColor ?? color,
+              mode: mode,
+              size: size,
+            ),
+        ],
       ),
     );
   }
 }
 
 class _JoystickBasePainter extends CustomPainter {
-  final Color arrowsColor;
   final Color color;
-  final bool drawArrows;
   final JoystickMode mode;
   final bool withBorderCircle;
 
   _JoystickBasePainter({
-    required this.arrowsColor,
     required this.color,
-    required this.drawArrows,
     required this.mode,
     required this.withBorderCircle,
   });
 
   static const double borderStrokeWidthPercentage = 0.05;
-  static const double arrowStrokeWidthPercentage = 0.025;
   static const double innerCircleRadiusReductionPercentage = 0.06;
   static const double outermostCircleRadiusReductionPercentage = 0.3;
-  static const double arrowWidth = 0.15;
-  static const double arrowHeight = 0.1;
-  static const double arrowHeadOffset = 0.35;
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -80,7 +84,6 @@ class _JoystickBasePainter extends CustomPainter {
     final center = Offset(radius, radius);
 
     drawCircles(canvas, center, diameter, withBorderCircle);
-    drawVerticalAndHorizontalArrows(canvas, center, diameter);
   }
 
   void drawCircles(
@@ -111,62 +114,6 @@ class _JoystickBasePainter extends CustomPainter {
       diameter / 2 - outermostCircleRadiusReductionPercentage * diameter,
       innerPaint,
     );
-  }
-
-  void drawVerticalAndHorizontalArrows(
-      Canvas canvas, Offset center, double diameter) {
-    final linePaint = Paint()
-      ..color = arrowsColor
-      ..strokeWidth = arrowStrokeWidthPercentage * diameter
-      ..style = PaintingStyle.stroke;
-    drawVerticalArrows(canvas, center, diameter, linePaint);
-    drawHorizontalArrows(canvas, center, diameter, linePaint);
-  }
-
-  void drawVerticalArrows(
-      Canvas canvas, Offset center, double diameter, Paint linePaint) {
-    if (drawArrows && mode != JoystickMode.horizontal) {
-      final height = arrowHeight * diameter;
-      final headOffset = arrowHeadOffset * diameter;
-      final width = arrowWidth * diameter;
-
-      final topArrowHeadOffset = center.dy - headOffset;
-      var topArrowHead = Offset(center.dx, topArrowHeadOffset);
-      canvas.drawLine(
-          topArrowHead, topArrowHead.translate(-width, height), linePaint);
-      canvas.drawLine(
-          topArrowHead, topArrowHead.translate(width, height), linePaint);
-
-      final bottomArrowHeadOffset = center.dy + headOffset;
-      var bottomArrowHead = Offset(center.dx, bottomArrowHeadOffset);
-      canvas.drawLine(bottomArrowHead,
-          bottomArrowHead.translate(-width, -height), linePaint);
-      canvas.drawLine(bottomArrowHead,
-          bottomArrowHead.translate(width, -height), linePaint);
-    }
-  }
-
-  void drawHorizontalArrows(
-      Canvas canvas, Offset center, double diameter, Paint linePaint) {
-    if (drawArrows && mode != JoystickMode.vertical) {
-      final height = arrowHeight * diameter;
-      final headOffset = arrowHeadOffset * diameter;
-      final width = arrowWidth * diameter;
-
-      final leftArrowHeadOffset = center.dx - headOffset;
-      final leftArrowHead = Offset(leftArrowHeadOffset, center.dy);
-      canvas.drawLine(
-          leftArrowHead, leftArrowHead.translate(height, -width), linePaint);
-      canvas.drawLine(
-          leftArrowHead, leftArrowHead.translate(height, width), linePaint);
-
-      final rightArrowHeadOffset = center.dx + headOffset;
-      var rightArrowHead = Offset(rightArrowHeadOffset, center.dy);
-      canvas.drawLine(
-          rightArrowHead, rightArrowHead.translate(-height, -width), linePaint);
-      canvas.drawLine(
-          rightArrowHead, rightArrowHead.translate(-height, width), linePaint);
-    }
   }
 
   @override
