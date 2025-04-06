85d3a540cfcb442f2f46258e0bfdfad16ff99003
Pavel Zaichyk <zaychick.pav@yandex.ru>
2025-04-06 12:58:32 +0200

    [#10] Cancel callback timer before starting a new periodic timer

--- lib/src/joystick.dart
+++ lib/src/joystick.dart
@@ -186,6 +186,7 @@ class _JoystickState extends State<Joystick> {
   }
 
   void _runCallback() {
+    _callbackTimer?.cancel();
     _callbackTimer = Timer.periodic(widget.period, (timer) {
       widget.listener(StickDragDetails(_stickOffset.dx, _stickOffset.dy));
     });
